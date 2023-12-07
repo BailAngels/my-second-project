d7a8212952fbf7261f821845ea53fefa788b6393
BailAngels <wolf.abmin.14@gmail.com>
2023-12-07 15:54:13 +0600

    My second project

---
+++ apps/blogs/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+
+from apps.blogs.views import index, create_blog, detail_blog, delete_blog, update_blog
+
+
+urlpatterns = [
+    path('', index, name="homepage"),
+    path('create/', create_blog, name='create_blog'),
+    path('blog/<int:pk>', detail_blog, name="detail_blog"),
+    path('delete/<int:pk>', delete_blog, name="delete_blog"),
+    path('update/<int:pk>', update_blog, name="update_blog"),
+]
